aba47fc6c3776bbd9c46be6dfb131c9989a04915
Tiago Martins <tiago.martins@beonesolutions.com>
2022-07-11 22:36:49 +0100

    Add project files.

---
+++ TimeManagementUtility/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TimeManagementUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.endBtn = new System.Windows.Forms.Button();
+            this.extendBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startBtn.Location = new System.Drawing.Point(12, 139);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(85, 50);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(231, 110);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 23);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 81);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(265, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(12, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(207, 23);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Task";
+            // 
+            // endBtn
+            // 
+            this.endBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.endBtn.Location = new System.Drawing.Point(102, 139);
+            this.endBtn.Name = "endBtn";
+            this.endBtn.Size = new System.Drawing.Size(85, 50);
+            this.endBtn.TabIndex = 8;
+            this.endBtn.Text = "Finish";
+            this.endBtn.UseVisualStyleBackColor = true;
+            this.endBtn.Click += new System.EventHandler(this.endBtn_Click);
+            // 
+            // extendBtn
+            // 
+            this.extendBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.extendBtn.Location = new System.Drawing.Point(192, 139);
+            this.extendBtn.Name = "extendBtn";
+            this.extendBtn.Size = new System.Drawing.Size(85, 50);
+            this.extendBtn.TabIndex = 9;
+            this.extendBtn.Text = "Extend";
+            this.extendBtn.UseVisualStyleBackColor = true;
+            this.extendBtn.Click += new System.EventHandler(this.extendBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(77, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(142, 65);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "00:00";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(289, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 184);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 200);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.extendBtn);
+            this.Controls.Add(this.endBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.startBtn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Time Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button startBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer;
+        private NumericUpDown numericUpDown1;
+        private ProgressBar progressBar1;
+        private ComboBox comboBox1;
+        private Button endBtn;
+        private Button extendBtn;
+        private Label label1;
+        private ListBox listBox1;
+    }
+}
---
+++ TimeManagementUtility/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System.ComponentModel;
+
+namespace TimeManagementUtility
+{
+    public partial class Form1 : Form
+    {
+        private int _ticks;
+        private int _maxTicks;
+        private bool _isTimerRunning = false;
+        BindingList<TimedTask> _items;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _items = new BindingList<TimedTask>();
+            listBox1.DataSource = _items;
+            listBox1.DisplayMember = "DisplayValue";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Break");
+            endBtn.Enabled = false;
+            extendBtn.Enabled = false;
+        }
+
+        private void startTimer()
+        {
+            _ticks = 0;
+            _maxTicks = 60 * Decimal.ToInt32(numericUpDown1.Value);
+            setLabels();
+            progressBar1.Maximum = _maxTicks;
+            progressBar1.Value = _maxTicks;
+
+            startBtn.Text = "Pause";
+            progressBar1.Enabled = true;
+            endBtn.Enabled = true;
+            extendBtn.Enabled = true;
+
+            timer.Start();
+            _isTimerRunning = true;
+            return;
+        }
+
+        private void clearTimer()
+        {
+            _ticks = 0;
+            label1.Text = $"00:00";
+            this.Text = "Finished";
+            startBtn.Text = "Start";
+            progressBar1.Value = 0;
+            progressBar1.Enabled = false;
+            endBtn.Enabled = false;
+            extendBtn.Enabled = false;
+            startBtn.Enabled = true;
+
+            timer.Stop();
+            _isTimerRunning = false;
+            return;
+        }
+
+        private void togglePause(bool isRunning)
+        {
+            if (isRunning)
+            {
+                startBtn.Text = "Resume";
+
+                timer.Stop();
+                _isTimerRunning = false;
+                return;
+            }
+
+            startBtn.Text = "Pause";
+
+            timer.Start();
+            _isTimerRunning = true;
+            return;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (!_isTimerRunning && _ticks == 0)
+            {
+                startTimer();
+            }
+            else
+            {
+                togglePause(_isTimerRunning);
+            }
+        }
+
+        private static string elapsedMinutes(int ticks)
+        {
+            int minutes = ((ticks) / 60);
+            return minutes == 60 ? "00" : (minutes < 10 ? $"0{minutes}" : minutes.ToString());
+        }
+
+        private static string elapsedSeconds(int ticks)
+        {
+            int seconds = (ticks % 60);
+            return seconds == 60 ? "00" : (seconds < 10 ? $"0{seconds}" : seconds.ToString());
+        }
+
+        private string remainingMinutes()
+        {
+            int minutes = ((_maxTicks - _ticks) / 60);
+            return minutes == 60 ? "00" : (minutes < 10 ? $"0{minutes}" : minutes.ToString());
+        }
+
+        private string remainingSeconds()
+        {
+            int seconds = (60 - (_ticks % 60));
+            return seconds == 60 ? "00" : (seconds < 10 ? $"0{seconds}" : seconds.ToString());
+        }
+
+        private void setLabels()
+        {
+
+            string minutesText = remainingMinutes();
+            string secondsText = remainingSeconds();
+            label1.Text = $"{minutesText}:{secondsText}";
+            this.Text = $"{minutesText}:{secondsText}";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            _ticks++;
+            progressBar1.Value--;
+            setLabels();
+
+            if (_ticks == _maxTicks)
+            {
+                timer.Stop();
+                _isTimerRunning = false;
+                startBtn.Enabled = false;
+            }
+        }
+
+        private void endBtn_Click(object sender, EventArgs e)
+        {
+            TimedTask timedTask = null;
+            if (comboBox1.SelectedItem == null)
+            {
+                int taskIndex = comboBox1.Items.Add(comboBox1.Text);
+                _items.Add(new TimedTask { Key = taskIndex, Ticks = _ticks, Name = comboBox1.Text });
+                comboBox1.SelectedItem = comboBox1.Items[taskIndex];
+            }
+            else
+            {
+                timedTask = _items.FirstOrDefault(item => item.Key == comboBox1.SelectedIndex);
+                if (timedTask != null)
+                {
+                    int index = _items.IndexOf(timedTask);
+                    _items.RemoveAt(index);
+                    _items.Add(TimedTask.CloneAndUpdate(timedTask, _ticks));
+                } 
+                else
+                {
+                    _items.Add(new TimedTask { Key = comboBox1.SelectedIndex, Ticks = _ticks, Name = comboBox1.Text });
+                }
+            }
+
+            listBox1.Update();
+            clearTimer();
+        }
+
+        private void extendBtn_Click(object sender, EventArgs e)
+        {
+            _maxTicks = _maxTicks + (60 * Decimal.ToInt32(numericUpDown1.Value));
+            progressBar1.Maximum = _maxTicks;
+            progressBar1.Value = _maxTicks - _ticks;
+            if (!_isTimerRunning)
+            {
+                timer.Start();
+                _isTimerRunning = true;
+                startBtn.Enabled = true;
+            }
+        }
+
+        private class TimedTask
+        {
+            public int Key { get; set; }
+            public string Name { get; set; }
+            public int Ticks { get; set; }
+            public string DisplayValue 
+            {
+                get { return $"{elapsedMinutes(Ticks)}:{elapsedSeconds(Ticks)} : {Name}"; }
+            }
+
+            public static TimedTask CloneAndUpdate(TimedTask oldTimedTask, int ticks)
+            {
+                TimedTask timedTask = new TimedTask();
+                timedTask.Key = oldTimedTask.Key;
+                timedTask.Name = oldTimedTask.Name;
+                timedTask.Ticks = oldTimedTask.Ticks + ticks;
+                return timedTask;
+            }
+        }
+    }
+}
